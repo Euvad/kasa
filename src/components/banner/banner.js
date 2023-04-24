cac8cfb4d62a098308d1cec251e8411f9d204e41
VAD <vad.studio@icloud.com>
2023-04-25 00:24:17 +0200

    lastcom

--- src/components/banner/banner.js
+++ src/components/banner/banner.js
@@ -1,11 +1,12 @@
 import './banner.scss';
 import bgimage1 from '../../assets/home_cover.png';
 import bgimage2 from '../../assets/about_cover.png';
+import { useLocation } from 'react-router-dom';
 
 export default function Banner() {
-  const path = window.location.pathname;
+  const path = useLocation();
 
-  if (path === "/") {
+  if (path.pathname === "/") {
     return (
     <div className="banner-container">
       <img className='banner-img' alt='bgimage' src={bgimage1}></img>
@@ -15,7 +16,7 @@ export default function Banner() {
     </div>
     );
   }
-  else if (path === "/about") {
+  else if (path.pathname === "/about") {
     return (
     <div className="banner-container-2">
       <img className='banner-img-2' alt='bgimage' src={bgimage2}></img>
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename='/ocr_webdev_p7'>
+  <BrowserRouter>
     <App />
   </BrowserRouter>
 );
