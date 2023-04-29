369f818a458a15d410df1de5f67636e6235b924c
VAD <vad.studio@icloud.com>
2023-04-29 19:26:52 +0200

    Rewrote navbar.js to fix the bug when navlistactive was not showing as active because react don't reload header, fixed this using Navlink from react router dom

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,24 +1,32 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import './navbar.scss'
 function Navbar() {
-
-    const currentRoute = window.location.pathname;
     return (
         <nav className='nav'>
-        <ul className='nav_list'>
-            <li className={currentRoute === '/' ? 'nav_list_item_active' : 'nav_list_item'}>
-                <Link  to='/'>
-                    Accueil
-                </Link>
-            </li>
-            <li className={currentRoute === '/about' ? 'nav_list_item_active' : 'nav_list_item'}>
-                <Link  to='/about'>
-                    A propos
-                </Link>
-            </li>
-        </ul>
-    </nav>
+            <ul className='nav_list'>
+                <li>
+                    <NavLink
+                        to="/"
+                        className={({ isActive, isPending }) =>
+                            isPending ? "pending" : isActive ? "nav_list_item_active" : ""
+                        }
+                    >
+                        Accueil
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to="/about"
+                        className={({ isActive, isPending }) =>
+                            isPending ? "pending" : isActive ? "nav_list_item_active" : ""
+                        }
+                    >
+                        A propos
+                    </NavLink>
+                </li>
+            </ul>
+        </nav>
     )
 }
 
